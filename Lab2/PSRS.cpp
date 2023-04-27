ea11a6ff70423e8497f49f90118aa19baae6e8f5
xxa_local <xuao123@mail.ustc.edu.cn>
2023-04-27 18:15:57 +0800

    finish

--- Lab2/PSRS.cpp
+++ Lab2/PSRS.cpp
@@ -10,7 +10,6 @@ int global_samples[NUM_THREADS * NUM_THREADS];
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
-
     int thread_num = NUM_THREADS;
     int id;
     MPI_Comm_size(MPI_COMM_WORLD, &thread_num);
@@ -59,13 +58,6 @@ int main(int argc, char* argv[])
         local_count[index]++;
         j++;
     }
-    printf("id = %d\n", id);
-    printf("local_count[0] = %d\n", local_count[0]);
-    for (int i = 0; i < local_count[0]; i++) {
-        printf("%d ", local_parts[0][i]);
-    }
-    printf("\n");
-
     // step 7. 全局交换
     // 每个线程 p_id 需要处理 NUM_THREADS 个 local_parts[id][] 
     int* send_buf = (int*)malloc(N * NUM_THREADS * sizeof(int));
@@ -74,20 +66,6 @@ int main(int argc, char* argv[])
     int* recv_counts = (int*)malloc(NUM_THREADS * sizeof(int));
     int* send_displs = (int*)malloc(NUM_THREADS * sizeof(int));
     int* recv_displs = (int*)malloc(NUM_THREADS * sizeof(int));
-    /*
-    for (int i = 0; i < NUM_THREADS; i++) {
-        for (int j = 0; j < local_count[i]; j++) {
-            send_buf[i * N + j] = local_parts[i][j];
-        }
-        send_counts[i] = local_count[i];
-    }
-    MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, MPI_COMM_WORLD);
-    send_displs[0] = recv_displs[0] = 0;
-    for (int i = 1; i < NUM_THREADS; i++) {
-        send_displs[i] = send_displs[i - 1] + send_counts[i - 1];
-        recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
-    }
-    */
     for (int i = 0; i < NUM_THREADS; i++) {
         send_counts[i] = local_count[i];
     }
@@ -103,55 +81,61 @@ int main(int argc, char* argv[])
         }
     }
     MPI_Alltoallv(send_buf, send_counts, send_displs, MPI_INT, recv_buf, recv_counts, recv_displs, MPI_INT, MPI_COMM_WORLD);
+    //printf("id = %d\n", id);
+    //for (int i = 0; i < recv_displs[NUM_THREADS - 1] + recv_counts[NUM_THREADS - 1]; i++) {
+    //    printf("%d  ", recv_buf[i]);
+    //}
+    //printf("\n");
+    //std::sort(recv_buf, recv_buf + N * NUM_THREADS);
+    //std::sort(recv_buf, recv_buf + recv_displs[NUM_THREADS - 1] + recv_counts[NUM_THREADS - 1]); 
 
-    /*
-    printf("id = %d\n", id);
-    for (int i = 0; i < NUM_THREADS; i++) {
-        printf("recv from %d  %d nums\n", i, recv_counts[i]);
-        for (int j = 0; j < recv_counts[i]; j++) {
-            printf("%d  %d\n", recv_displs[i]+j, recv_buf[recv_displs[i] + j]);
+    // 至此，完成全局交换
+    // 每个线程有一个一维数组 recv_buf,按 recv_couts 分段有序（本质上是因为来源于全局交换）
+    // step 8. 归并排序
+    // 将 recv_buf 分段 merge，最终写入到 A 数组
+    int result[N] = { 0 };
+    int tmp[N] = { 0 };
+    int result_count = 0, tmp_count = 0;
+    for (int index = 0; index < NUM_THREADS; index++) {
+        // Merge: result[] 和 parts[index][id][]
+        int i = 0, jj = 0, k = 0;
+        int A_count = result_count;
+        int B_count = recv_counts[index];
+        tmp_count = A_count + B_count;
+        while (i < A_count && jj < B_count) {
+            if (result[i] <= recv_buf[recv_displs[index] + jj]) tmp[k++] = result[i++];
+            else {
+                tmp[k++] = recv_buf[recv_displs[index] + jj];
+                jj++;
+            }
         }
-        printf("\n");
-    }
-    printf("\n");
-    */
-    /*
-
-    int result[N];
-    int result_count = 0;
-    for (int i = 0; i < NUM_THREADS; i++) {
-        int recvbuf[N];
-        int recvcount;
-        MPI_Gather(local_count + i, 1, MPI_INT, &recvcount, 1, MPI_INT, i, MPI_COMM_WORLD);
-        MPI_Gatherv(local_parts[i], local_count[i], MPI_INT, recvbuf, &recvcount, NULL, MPI_INT, i, MPI_COMM_WORLD);
-
-        if (world_rank == i) {
-            int p1 = 0, p2 = 0, p = 0;
-            while (p1 < result_count && p2 < recvcount) {
-                if (result[p1] <= recvbuf[p2]) {
-                    A[p++] = result[p1++];
-                }
-                else {
-                    A[p++] = recvbuf[p2++];
-                }
-            }
-            while (p1 < result_count) {
-                A[p++] = result[p1++];
-            }
-            while (p2 < recvcount) {
-                A[p++] = recvbuf[p2++];
-            }
-            result_count += recvcount;
-            memcpy(result, A, result_count * sizeof(int));
+        while (i < A_count) tmp[k++] = result[i++];
+        while (jj < B_count) {
+            tmp[k++] = recv_buf[recv_displs[index] + jj];
+            jj++;
+        }
+        result_count = tmp_count;
+        for (int l = 0; l < tmp_count; l++) {
+            result[l] = tmp[l];
         }
     }
-    if (world_rank == 0) {
+    // 这里出过一个 BUG，差不多调了 1.5 小时，结果发现是上面的该写为 index 的地方写为了 i
+    int* A_counts = (int*)malloc(NUM_THREADS * sizeof(int));
+    int* A_displs = (int*)malloc(NUM_THREADS * sizeof(int));
+    MPI_Gather(&result_count, 1, MPI_INT, A_counts, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (id == 0) {
+        A_displs[0] = 0;
+        for (int i = 1; i < NUM_THREADS; i++) {
+            A_displs[i] = A_displs[i - 1] + A_counts[i - 1];
+        }
+    }
+    MPI_Gatherv(result, result_count, MPI_INT, A, A_counts, A_displs, MPI_INT, 0, MPI_COMM_WORLD);
+    if (id == 0) {
         for (int i = 0; i < N; i++) {
             printf("%d ", A[i]);
         }
         printf("\n");
     }
-    */
     MPI_Finalize();
     return 0;
 }
