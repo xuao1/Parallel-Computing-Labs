fb71a14a3f751a6b9c8b4a6639b4c9be9061ad47
xxa_local <xuao123@mail.ustc.edu.cn>
2023-04-27 17:10:43 +0800

    finish task7

--- Lab2/PSRS.cpp
+++ Lab2/PSRS.cpp
@@ -26,50 +26,100 @@ int main(int argc, char* argv[])
     for (int j = 0; j < NUM_THREADS; j++) {
         samples[j] = A[pstart + j * step];
     }
-    // step 4. 采样排序
-    MPI_Allgather(samples, NUM_THREADS, MPI_INT, global_samples + id * NUM_THREADS, NUM_THREADS, MPI_INT, MPI_COMM_WORLD);
+    // 全局 samples 只需要线程 0 获取即可，它从这些数据中选择主元
+    // 再将主元广播到各个线程
+    int* global_samples = (int*)malloc(NUM_THREADS * NUM_THREADS * sizeof(int));
+    int* pivots = (int*)malloc(NUM_THREADS * sizeof(int));
+    MPI_Gather(samples, NUM_THREADS, MPI_INT, global_samples, NUM_THREADS, MPI_INT, 0, MPI_COMM_WORLD);
     if (id == 0) {
+        // step 4. 采样排序
         std::sort(global_samples, global_samples + NUM_THREADS * NUM_THREADS);
-    }
-    for (int i = 0; i < NUM_THREADS * NUM_THREADS; i++) {
-        printf("%d ", global_samples[i]);
-    }
-    /*
-    int pivots[NUM_THREADS - 1];
-    if (world_rank == 0) {
-        for (int i = 0; i < world_size - 1; i++) {
-            pivots[i] = global_samples[(i + 1) * world_size];
+        // step 5. 选择主元
+        for (int i = 0; i < NUM_THREADS - 1; i++) {
+            pivots[i] = global_samples[(i + 1) * NUM_THREADS];
         }
     }
-
-    MPI_Bcast(pivots, world_size - 1, MPI_INT, 0, MPI_COMM_WORLD);
-
+    MPI_Bcast(pivots, NUM_THREADS - 1, MPI_INT, 0, MPI_COMM_WORLD);
+    // step 6. 主元划分
     int index = 0;
     int j = pstart;
-
     int local_parts[NUM_THREADS][N] = { 0 };
     int local_count[NUM_THREADS] = { 0 };
-
-    while (j < pend && index < world_size - 1) {
-        if (A[j] < pivots[index]) {
+    while (j < pend && index < NUM_THREADS - 1) {
+        if (A[j] <= pivots[index]) {
             local_parts[index][local_count[index]] = A[j];
-            local_count[index]++;
+            local_count[index]++; // <= pivot[index] 的有 count[index] 个
             j++;
         }
-        else {
-            index++;
-        }
+        else index++;
     }
-
+    // 大于最后一个主元的部分：此时 index = NUM_THREADS - 1
     while (j < pend) {
         local_parts[index][local_count[index]] = A[j];
         local_count[index]++;
         j++;
     }
+    printf("id = %d\n", id);
+    printf("local_count[0] = %d\n", local_count[0]);
+    for (int i = 0; i < local_count[0]; i++) {
+        printf("%d ", local_parts[0][i]);
+    }
+    printf("\n");
+
+    // step 7. 全局交换
+    // 每个线程 p_id 需要处理 NUM_THREADS 个 local_parts[id][] 
+    int* send_buf = (int*)malloc(N * NUM_THREADS * sizeof(int));
+    int* recv_buf = (int*)malloc(N * NUM_THREADS * sizeof(int));
+    int* send_counts = (int*)malloc(NUM_THREADS * sizeof(int));
+    int* recv_counts = (int*)malloc(NUM_THREADS * sizeof(int));
+    int* send_displs = (int*)malloc(NUM_THREADS * sizeof(int));
+    int* recv_displs = (int*)malloc(NUM_THREADS * sizeof(int));
+    /*
+    for (int i = 0; i < NUM_THREADS; i++) {
+        for (int j = 0; j < local_count[i]; j++) {
+            send_buf[i * N + j] = local_parts[i][j];
+        }
+        send_counts[i] = local_count[i];
+    }
+    MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, MPI_COMM_WORLD);
+    send_displs[0] = recv_displs[0] = 0;
+    for (int i = 1; i < NUM_THREADS; i++) {
+        send_displs[i] = send_displs[i - 1] + send_counts[i - 1];
+        recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
+    }
+    */
+    for (int i = 0; i < NUM_THREADS; i++) {
+        send_counts[i] = local_count[i];
+    }
+    MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, MPI_COMM_WORLD);
+    send_displs[0] = recv_displs[0] = 0;
+    for (int i = 1; i < NUM_THREADS; i++) {
+        send_displs[i] = send_displs[i - 1] + send_counts[i - 1];
+        recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
+    }
+    for (int i = 0; i < NUM_THREADS; i++) {
+        for (int j = 0; j < send_counts[i]; j++) {
+            send_buf[send_displs[i] + j] = local_parts[i][j];
+        }
+    }
+    MPI_Alltoallv(send_buf, send_counts, send_displs, MPI_INT, recv_buf, recv_counts, recv_displs, MPI_INT, MPI_COMM_WORLD);
+
+    /*
+    printf("id = %d\n", id);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        printf("recv from %d  %d nums\n", i, recv_counts[i]);
+        for (int j = 0; j < recv_counts[i]; j++) {
+            printf("%d  %d\n", recv_displs[i]+j, recv_buf[recv_displs[i] + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    */
+    /*
 
     int result[N];
     int result_count = 0;
-    for (int i = 0; i < world_size; i++) {
+    for (int i = 0; i < NUM_THREADS; i++) {
         int recvbuf[N];
         int recvcount;
         MPI_Gather(local_count + i, 1, MPI_INT, &recvcount, 1, MPI_INT, i, MPI_COMM_WORLD);
