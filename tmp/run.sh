f12c3a1b16c6d99f37df8832e1d15686a3912c65
xxa_local <xuao123@mail.ustc.edu.cn>
2023-05-18 21:52:02 +0800

    update

--- tmp/run.sh
+++ tmp/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-timeout echo "input0.txt" | ./Lab1
-timeout echo "input1.txt" | ./Lab1
-timeout echo "input2.txt" | ./Lab1
-timeout echo "input3.txt" | ./Lab1
-timeout echo "input4.txt" | ./Lab1
-timeout echo "input5.txt" | ./Lab1
-timeout echo "input6.txt" | ./Lab1
-timeout echo "input7.txt" | ./Lab1
-timeout echo "input8.txt" | ./Lab1
-timeout echo "input9.txt" | ./Lab1
+timeout 1m  echo "input0.txt" | ./Lab1
+timeout 1m echo "input1.txt" | ./Lab1
+timeout 1m echo "input2.txt" | ./Lab1
+timeout 1m echo "input3.txt" | ./Lab1
+timeout 1m echo "input4.txt" | ./Lab1
+timeout 1m echo "input5.txt" | ./Lab1
+timeout 1m echo "input6.txt" | ./Lab1
+timeout 1m echo "input7.txt" | ./Lab1
+timeout 1m echo "input8.txt" | ./Lab1
+timeout 1m echo "input9.txt" | ./Lab1
