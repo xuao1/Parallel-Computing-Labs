038f9918df98a4d164139d5d8c54803be538e946
xxa_local <xuao123@mail.ustc.edu.cn>
2023-05-18 21:46:34 +0800

    update

--- tmp/run.sh
+++ tmp/run.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-echo "input0.txt" | ./Lab1
-echo "input1.txt" | ./Lab1
-echo "input2.txt" | ./Lab1
-echo "input3.txt" | ./Lab1
-echo "input4.txt" | ./Lab1
-echo "input5.txt" | ./Lab1
-echo "input6.txt" | ./Lab1
-echo "input7.txt" | ./Lab1
-echo "input8.txt" | ./Lab1
-echo "input9.txt" | ./Lab1
+#!/bin/bash
+
+timeout 10m ./Lab1 input0.txt
+timeout 10m ./Lab1 input1.txt
+timeout 10m ./Lab1 input2.txt
+timeout 10m ./Lab1 input3.txt
+timeout 10m ./Lab1 input4.txt
+timeout 10m ./Lab1 input5.txt
+timeout 10m ./Lab1 input6.txt
+timeout 10m ./Lab1 input7.txt
+timeout 10m ./Lab1 input8.txt
+timeout 10m ./Lab1 input9.txt
